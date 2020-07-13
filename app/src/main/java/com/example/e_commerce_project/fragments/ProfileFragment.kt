dbe822643c143e1f4a53a06bade99caffa0798a8
Aknur Toktarov <aknurr999@gmail.com>
2020-07-13 15:12:21 +0600

    login registration profile

--- app/src/main/java/com/example/e_commerce_project/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/e_commerce_project/fragments/ProfileFragment.kt
@@ -1,19 +1,40 @@
 package com.example.e_commerce_project.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
+import com.example.e_commerce_project.LoginActivity
 import com.example.e_commerce_project.R
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var logout: AppCompatButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews(view)
+
+        logout.setOnClickListener {
+            val intent = Intent(requireActivity(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+    }
+
+    fun bindViews(view: View) {
+        logout = view.findViewById(R.id.logout)
     }
 
 }
